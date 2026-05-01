4c4b954d9c17d1c4dd08ebfb7c951057db614531
Yara Essam Ellkany <117939379+Yara-Ellkany@users.noreply.github.com>
2026-05-01 14:54:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 import streamlit as st
 from gtts import gTTS
 import io
- 
+
 st.title(" حول النص إلى صوت!")
- 
+
 text = st.text_area("اكتب النص هنا:")
- 
+
 lang = st.selectbox("اختار اللغة:", ["عربي", "إنجليزي"])
- 
+
+speed = st.selectbox("اختار سرعة الصوت:", [" سريع", " عادي", "🐢 بطيء"])
+
 if st.button("تشغيل الصوت "):
     if text:
         lang_code = "ar" if lang == "عربي" else "en"
-        tts = gTTS(text=text, lang=lang_code)
+        is_slow = speed == " بطيء"
+        tts = gTTS(text=text, lang=lang_code, slow=is_slow)
         buf = io.BytesIO()
         tts.write_to_fp(buf)
         buf.seek(0)
         st.audio(buf, format="audio/mp3")
     else:
-        st.warning("اكتب نص أولاً ")
- 
+        st.warning("اكتب نص أولاً! ")
